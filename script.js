6f90be8c5792c682385de88035582d9a99b024bf
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-01 11:42:40 +0200

    Personality traits changed

---
+++ script.js
@@ -0,0 +1,8 @@
+// Personality traits
+
+let traits = document.getElementById("traits").innerHTML;
+
+document.getElementById("traits").innerHTML = "Cynical<br>Sarcastic<br>Sociable";
+
+console.log(traits);
+
