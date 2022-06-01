cbd39bc7a668cd4f6fb6a875445065429cd5e816
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-01 19:17:54 +0200

    Bowling image and quote changed

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ let traits = document.getElementById("traits").innerHTML;
 document.getElementById("traits").innerHTML = "Stubborn<br>Aggressive<br>mentally unhinged";
 
 
+// Quote
+
+document.querySelector("q").innerHTML = "You want a toe? I can get you a toe, believe me. With nail polish."
+
 
 // Profile picture
 
@@ -30,10 +34,23 @@ document.body.appendChild(editCSS);
 
 // Bowling picture
 
+let editCSSTwo = document.createElement("style")
+editCSSTwo.innerHTML = ".hockey {background: url(../img/bowling.jpg)}";
+document.body.appendChild(editCSSTwo);
+
+//let bowlingPic = document.createElement("style").bowlingPic.innerHTML = ".hockey {background: url(../img/sobchak bowling copy.jpg)}"
 
 
 // Bowling text
 
-document.getElementById("sportDescription").innerHTML = "“Smokey, this is not 'Nam. This is bowling. There are rules. You mark that frame an 8, and you're entering a world of pain.”"
+document.getElementById("sportDescription").innerHTML = "“Smokey, this is not 'Nam. This is bowling. There are rules. You mark that frame an 8, and you're entering a world of pain.”";
+
+
+// Colour text
+
+document.getElementById("colourDescription").innerHTML = "I actually love the summer, don't have a favourite color however so Bordeaux is fine by me!"
+
+
+
 
 
