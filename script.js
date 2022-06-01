6a6cc42d483d6c770d8387e21c03461b4ee4ef3f
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-01 16:08:39 +0200

    FINALLY changed profile picture,,thanks stack overflow

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // Personality traits
 
+const words = document.getElementsByClassName("words")[0];
+console.log(words);
+
+words.setAttribute("id", "traits");
+
 let traits = document.getElementById("traits").innerHTML;
 
 document.getElementById("traits").innerHTML = "Stubborn<br>Aggressive<br>mentally unhinged";
 
-console.log(traits);
+
 
 // Profile picture
 
-document.getElementById("profilepic").src = "img/walter.jpg"; 
+
+let editCSS = document.createElement("style")
+editCSS.innerHTML = ".cedric {background: url(../img/walter.jpg)}";
+document.body.appendChild(editCSS);
+
+
+//let cedric = document.getElementsByClassName("cedric")[0];
+//console.log(cedric);
+
+//cedric[0].setAttribute("background", "url(../img/walter.jpg)");
+
+//cedric.src = "../img/walter.jpg"
+
 
 
 // Bowling picture
