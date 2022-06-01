82e5aae2e3795e540d6dbc6f067e7f33987e0458
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-01 14:15:09 +0200

    innerhtml of paragraph changed

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 
 let traits = document.getElementById("traits").innerHTML;
 
-document.getElementById("traits").innerHTML = "Cynical<br>Sarcastic<br>Sociable";
+document.getElementById("traits").innerHTML = "Stubborn<br>Aggressive<br>mentally unhinged";
 
 console.log(traits);
+
+// Profile picture
+
+document.getElementById("profilepic").src = "img/walter.jpg"; 
+
+
+// Bowling picture
+
+
+
+// Bowling text
+
+document.getElementById("sportDescription").innerHTML = "“Smokey, this is not 'Nam. This is bowling. There are rules. You mark that frame an 8, and you're entering a world of pain.”"
+
 
